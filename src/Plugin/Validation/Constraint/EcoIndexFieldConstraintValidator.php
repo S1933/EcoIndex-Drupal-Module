4fb0ff0e0673f28c1afdaeb28547c2285413020c
S1933 <47284-jenue@users.noreply.drupalcode.org>
2024-08-10 15:05:18 +0100

    fix constraint validation

--- src/Plugin/Validation/Constraint/EcoIndexFieldConstraintValidator.php
+++ src/Plugin/Validation/Constraint/EcoIndexFieldConstraintValidator.php
@@ -17,19 +17,21 @@ class EcoIndexFieldConstraintValidator extends ConstraintValidator {
   public function validate($value, Constraint $constraint) {
     /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
     $entity = $this->context->getRoot()->getValue();
-    $ecoindx_settings = \Drupal::config('ecoindex.settings');
-    $minimum_score = $ecoindx_settings->get('minimum_score');
-    $required_to_publish = $ecoindx_settings->get('required_to_publish');
-
-    if ($required_to_publish === TRUE &&
-      $minimum_score > $value->getValue() &&
-      $entity instanceof EntityPublishedInterface &&
-      $entity->isPublished()
-    ) {
-    $this->context->addViolation($constraint->needsValue, [
-      '%$value' => $minimum_score,
-      '%field' => $value->getFieldDefinition()->getLabel(),
-    ]);
+    if ($entity instanceof EntityPublishedInterface) {
+      $ecoindx_settings = \Drupal::config('ecoindex.settings');
+      $minimum_score = $ecoindx_settings->get('minimum_score');
+      $required_to_publish = $ecoindx_settings->get('required_to_publish');
+      $score = $value->getScore();
+      if ($entity->isPublished() &&
+        $required_to_publish === TRUE &&
+        $score > 0 &&
+        $minimum_score > $score
+      ) {
+        $this->context->addViolation($constraint->needsValue, [
+          '%value' => $minimum_score,
+          '%field' => $value->getFieldDefinition()->getName(),
+        ]);
+      }
     }
   }
 
