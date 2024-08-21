b62c9c2a851c6d7e752afe99bebd92d7b2a6a021
S1933 <47284-jenue@users.noreply.drupalcode.org>
2024-08-21 21:59:34 +0100

    fix ecoindex.js

--- js/ecoindex.js
+++ js/ecoindex.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 /*
  *  Copyright (C) 2019 - 2022  didierfred@gmail.com
  *
@@ -17,18 +15,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const quantiles_dom = [
-  0, 47, 75, 159, 233, 298, 358, 417, 476, 537, 603, 674, 753, 843, 949, 1076,
-  1237, 1459, 1801, 2479, 594601,
-];
-const quantiles_req = [
-  0, 2, 15, 25, 34, 42, 49, 56, 63, 70, 78, 86, 95, 105, 117, 130, 147, 170,
-  205, 281, 3920,
-];
+const quantiles_dom = [0, 47, 75, 159, 233, 298, 358, 417, 476, 537, 603, 674, 753, 843, 949, 1076, 1237, 1459, 1801, 2479, 594601];
+const quantiles_req = [0, 2, 15, 25, 34, 42, 49, 56, 63, 70, 78, 86, 95, 105, 117, 130, 147, 170, 205, 281, 3920];
 const quantiles_size = [
-  0, 1.37, 144.7, 319.53, 479.46, 631.97, 783.38, 937.91, 1098.62, 1265.47,
-  1448.32, 1648.27, 1876.08, 2142.06, 2465.37, 2866.31, 3401.59, 4155.73,
-  5400.08, 8037.54, 223212.26,
+  0, 1.37, 144.7, 319.53, 479.46, 631.97, 783.38, 937.91, 1098.62, 1265.47, 1448.32, 1648.27, 1876.08, 2142.06, 2465.37, 2866.31, 3401.59, 4155.73, 5400.08,
+  8037.54, 223212.26,
 ];
 
 /**
@@ -45,9 +36,7 @@ function computeEcoIndex(dom, req, size) {
 function computeQuantile(quantiles, value) {
   for (let i = 1; i < quantiles.length; i++) {
     if (value < quantiles[i]) {
-      return (
-        i - 1 + (value - quantiles[i - 1]) / (quantiles[i] - quantiles[i - 1])
-      );
+      return i - 1 + (value - quantiles[i - 1]) / (quantiles[i] - quantiles[i - 1]);
     }
   }
   return quantiles.length - 1;
@@ -55,24 +44,24 @@ function computeQuantile(quantiles, value) {
 
 function getEcoIndexGrade(ecoIndex) {
   if (ecoIndex > 80) {
-    return 'A';
+    return "A";
   }
   if (ecoIndex > 70) {
-    return 'B';
+    return "B";
   }
   if (ecoIndex > 55) {
-    return 'C';
+    return "C";
   }
   if (ecoIndex > 40) {
-    return 'D';
+    return "D";
   }
   if (ecoIndex > 25) {
-    return 'E';
+    return "E";
   }
   if (ecoIndex > 10) {
-    return 'F';
+    return "F";
   }
-  return 'G';
+  return "G";
 }
 
 function computeGreenhouseGasesEmissionfromEcoIndex(ecoIndex) {
